8f5dcd4ca62eb1dfda16b9a54e8aee463eb90804
sonyps5201314 <sonyps5201314@gmail.com>
2023-01-14 22:20:32 +0800

    Fix the problem that the candidate box cannot pop up when entering Chinese in macOS 10.15 and above systems under the VMware virtual machine https://bugs.winehq.org/show_bug.cgi?id=47990

--- dlls/winemac.drv/cocoa_app.m
+++ dlls/winemac.drv/cocoa_app.m
@@ -213,6 +213,11 @@ static NSString* WineLocalizedString(unsigned int stringID)
         }
         [super dealloc];
     }
+    
+    - (void)applicationDidFinishLaunching:(NSNotification *)notification
+    {
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
+    }
 
     - (void) transformProcessToForeground:(BOOL)activateIfTransformed
     {
