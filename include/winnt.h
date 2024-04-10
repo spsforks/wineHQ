wine-mirror/wine

commit b3c989e6b249e135c644a263cf86bac0c461384a

Added some more useful macros.

include/winnt.h
@@ -282,6 +282,68 @@
 #define DUMMYUNIONNAME8  u8
 #endif /* !defined(NONAMELESSUNION) */
 
+/* C99 restrict support */
+
+#if defined(ENABLE_RESTRICTED) && !defined(MIDL_PASS) && !defined(RC_INVOKED)
+# if defined(_MSC_VER) && defined(_M_MRX000)
+#  define RESTRICTED_POINTER __restrict
+# elif defined(__GNUC__) && ((__GNUC__ > 2) || ((__GNUC__ == 2) && (__GNUC_MINOR__ >= 95)))
+#  define RESTRICTED_POINTER __restrict
+# else
+#  define RESTRICTED_POINTER
+# endif
+#else
+# define RESTRICTED_POINTER
+#endif
+
+/* C99 unaligned support */
+
+#if defined(_MSC_VER) && (defined(_M_MRX000) || defined(_M_ALPHA) || defined(_M_PPC) || defined(_M_IA64) || defined(_M_AMD64))
+# define UNALIGNED __unaligned
+# ifdef _WIN64
+#  define UNALIGNED64 __unaligned
+# else
+#  define UNALIGNED64
+# endif
+#else
+# define UNALIGNED
+# define UNALIGNED64
+#endif
+
+/* Alignment macros */
+
+#if defined(_WIN64) || (defined(_MSC_VER) && defined(_M_ALPHA)) || defined(__alpha__)
+#define MAX_NATURAL_ALIGNMENT sizeof(ULONGLONG)
+#define MEMORY_ALLOCATION_ALIGNMENT 16
+#else
+#define MAX_NATURAL_ALIGNMENT sizeof(DWORD)
+#define MEMORY_ALLOCATION_ALIGNMENT 8
+#endif
+
+#if (_MSC_VER >= 1300) && defined(__cplusplus)
+# define TYPE_ALIGNMENT(t) __alignof(t)
+#elif defined(__GNUC__)
+# define TYPE_ALIGNMENT(t) __alignof__(t)
+#else
+# define TYPE_ALIGNMENT(t) FIELD_OFFSET(struct { char x; t test; }, test)
+#endif
+
+#ifdef _WIN64
+# define PROBE_ALIGNMENT(_s) \
+    (TYPE_ALIGNMENT(_s) > TYPE_ALIGNMENT(DWORD) ? \
+    TYPE_ALIGNMENT(_s) : TYPE_ALIGNMENT(DWORD))
+# define PROBE_ALIGNMENT32(_s) TYPE_ALIGNMENT(DWORD)
+#else
+# define PROBE_ALIGNMENT(_s) TYPE_ALIGNMENT(DWORD)
+#endif
+
+/* Compile time assertion */
+
+#if defined(_MSC_VER)
+# define C_ASSERT(e) typedef char __C_ASSERT__[(e)?1:-1]
+#elif defined(__GNUC__) 
+# define C_ASSERT(e) extern char __C_ASSERT__[(e)?1:-1]
+#endif
 
 /**** Parts of windef.h that are needed here *****/
 
@@ -630,7 +692,7 @@ typedef struct _MEMORY_BASIC_INFORMATION
 typedef struct _LIST_ENTRY {
   struct _LIST_ENTRY *Flink;
   struct _LIST_ENTRY *Blink;
-} LIST_ENTRY, *PLIST_ENTRY;
+} LIST_ENTRY, *PLIST_ENTRY, * RESTRICTED_POINTER PRLIST_ENTRY;
 
 typedef struct _SINGLE_LIST_ENTRY {
   struct _SINGLE_LIST_ENTRY *Next;

spsforks/wineHQ

commit 7fde8c717261b19f4f939aaf7f648503ec24504c

include: Fix TYPE_ALIGNMENT() on GCC and Clang.
__alignof__() is unsuitable for TYPE_ALIGNMENT() implementation: it
returns the "preferred" alignment (on-stack alignment for automatic
variables) which may be bigger than the standard minimum alignment
(pointer alignment).

| Target Arch | Type Name | Standard  | __alignof__() |
| / platform  | (C ABI)   | alignment |               |
|-------------|-----------|-----------|---------------|
| i386 / SysV | long long |         4 |             8 |
| i386 / SysV | double    |         4 |             8 |

Fix this by using _Alignof(), or the offsetof() fallback on older
GCC/Clang versions.

Fixes: b3c989e6b24 (Added some more useful macros., 2002-10-02)

include/winnt.h
@@ -401,10 +401,29 @@ extern "C" {
 #define MEMORY_ALLOCATION_ALIGNMENT 8
 #endif
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1300) && defined(__cplusplus)
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L)
+# if (defined(__clang__) && (__clang_major__ < 8)) || ((__GNUC__ == 4) && (__GNUC_MINOR__ < 9) && !defined(__clang__))
+/*
+ * _Alignof() is noncompliant on GCC <4.9 as well as Clang <8.0: it returns the
+ * "preferred" alignment (on-stack alignment for automatic variables) which may
+ * be bigger than the standard minimum alignment (pointer alignment).
+ *
+ * | Target Arch | Type Name |      _Alignof()     | Standard  |
+ * | / platform  | (C ABI)   | GCC <4.8 | GCC 4.9+ | alignment |
+ * |-------------|-----------|----------|----------|-----------|
+ * | i386 / SysV | long long |        8 |        4 |         4 |
+ * | i386 / SysV | double    |        8 |        4 |         4 |
+ *
+ * See also:
+ * - GCC bug 52023 (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52023)
+ * - LLVM issue #26921 (https://github.com/llvm/llvm-project/issues/26921)
+ */
+#  define TYPE_ALIGNMENT(t) offsetof(struct { char x; t test; }, test)
+# else
+#  define TYPE_ALIGNMENT(t) _Alignof(t)
+# endif
+#elif defined(_MSC_VER) && (_MSC_VER >= 1300) && defined(__cplusplus)
 # define TYPE_ALIGNMENT(t) __alignof(t)
-#elif defined(__GNUC__)
-# define TYPE_ALIGNMENT(t) __alignof__(t)
 #else
 # define TYPE_ALIGNMENT(t) offsetof(struct { char x; t test; }, test)
 #endif